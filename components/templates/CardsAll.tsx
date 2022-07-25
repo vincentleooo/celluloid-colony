42525b657c5e0b8827ee9e866430851c1bc2fecd
Vincent Leonardo <vincentleoooo@icloud.com>
2022-07-25 12:54:26 +0800

    fix: missing title

--- components/templates/CardsAll.tsx
+++ components/templates/CardsAll.tsx
@@ -7,13 +7,13 @@ const CardsAll = () => {
     <>
     <Masonry breakpointCols={3} className="gap-6 hidden sm:flex">
       {dataJson.map(e => (
-      <div key={e.title} className="mb-6"><Cards link={"resources/" + e.path} chapter={e.chapter} title={e.title} image={e.image} attachment={e.Attachments ? e.Attachments : ""}></Cards></div>  
+      <div key={e.title} className="mb-6"><Cards link={"resources/" + e.path} chapter={e.chapter} title={e.title} image={e.image} attachment={e.Attachments ? e.Attachments : ""}></Cards></div>
       )
     )}
     </Masonry>
     <Masonry breakpointCols={1} className="gap-6 flex sm:hidden">
       {dataJson.map(e => (
-      <div key={e.title} className="mb-6"><Cards link={"resources/" + e.path} chapter={e.chapter} title={e.title} image={e.image} attachment={e.Attachments ? e.Attachments : ""}></Cards></div>  
+      <div key={e.title} className="mb-6"><Cards link={"resources/" + e.path} chapter={e.chapter} title={e.title} image={e.image} attachment={e.Attachments ? e.Attachments : ""}></Cards></div>
       )
     )}
     </Masonry>
