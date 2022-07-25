9d174741feed0addd666cc841c13c0893dd4e416
Vincent Leonardo <vincentleoooo@icloud.com>
2022-07-25 13:21:49 +0800

    feat: fallback for failed iframes

--- pages/resources/[id].tsx
+++ pages/resources/[id].tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 // pages/posts/[id].js
+import Link from "next/link";
 import dataJson from "../../components/data/data.json";
 
 type paramsDataID = {
@@ -44,7 +45,7 @@ export default function Post(props: postProps) {
       <h1 className="font-bold text-3xl text-center mb-1">{props.title}</h1>
       <p className="text-center">Chapter {props.chapter.toString()}</p>
       {props.attachment ? <img className="mx-auto my-6"src={props.attachment} alt="" /> : <></>}
-      {props.url ? <iframe className="w-full h-96 mx-auto my-6" src={props.url}></iframe> : <></>}
+      {props.url ? <><div className="my-6">Here is the <Link href={props.url}><a className="text-yellow-500 hover:text-yellow-700">link</a></Link> if the frame below does not show.</div><iframe className="w-full h-96 mx-auto my-3" src={props.url}></iframe></> : <></>}
     </div>
   );
 }
